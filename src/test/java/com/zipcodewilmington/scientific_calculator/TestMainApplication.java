a599e711644f7bdefd2e3ff52cd41da666515ef3
Cwarrick10 <warrickchar@gmail.com>
2022-06-27 21:48:10 -0400

    add static

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,46 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 2/9/18.
  */
+
 public class TestMainApplication {
+
+  @Before
+  public void setUp() {
+    class Calculator {
+
+    }
+      public double add(){
+
+        double c = 7;
+        double  n = 3;
+        double expected = c + n;
+
+        @Test
+      public double subtract(){
+        double c = 7;
+        double  n = 3;
+        double expected = c - n;
+      }
+      @Test
+      public double multiply(){
+
+        double c = 7;
+        double  n = 3;
+        double expected = c * n;
+      }
+      @Test
+      public double divide (){
+
+        double c = 7;
+        double  n = 3;
+        double expected = c / n;
+        return c;
+      }
+  }
 }
